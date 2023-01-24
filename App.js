2175208e0f069836d37e3ddbdec8a052c407d312
SobujDasCseBu <88959298+SobujDasCseBu@users.noreply.github.com>
2023-01-25 01:05:09 +0600

    Home page design

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
-import React from 'react'
-import { TouchableOpacity, StyleSheet, View, Text } from 'react-native'
+import React, { useState } from "react";
 import { Size } from 'react-native-ui-lib/src/components/skeletonView'
+import { StatusBar } from "expo-status-bar";
+
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 
 const App = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
    return (
      
          <View style={{backgroundColor: 'white',width: '100%',
@@ -10,26 +23,28 @@ const App = () => {
             <View style={{
                     width: '100%',
                     height: '8%',
-                    backgroundColor: '#b89558',
+                    backgroundColor: '#006A4E',
                     marginTop: '6%',
                     borderWidth: 1,
                     borderColor: "thistle",
                     borderRadius: 2,
+                   
 
                   }}>
                      <Text style ={{
                          paddingLeft: '30%',
                          paddingTop:10,
                          fontSize: 20,
+                         color: '#fff',
                           fontWeight: 'bold',
                      }} >Home Page</Text>
-              </View>
+            </View>
 
-              <View style={{
+            <View style={{
                     width: '90%',
-                    height: '40%',
-                    backgroundColor: '#b89558',
-                    marginTop: '30%',
+                    height: '50%',
+                    backgroundColor: '#006A4E',
+                    marginTop: '20%',
                     marginLeft: '5%',
                     borderWidth: 1,
                     borderColor: "thistle",
@@ -39,18 +54,86 @@ const App = () => {
                      <Text style ={{
                          paddingLeft: '30%',
                          paddingTop:10,
+                         paddingBottom:10,
                          fontSize: 20,
                           fontWeight: 'bold',
-                     }} >Home Page</Text>
-              </View>
+                          color: '#fff',
+                     }} >Login page</Text>
 
+                     <View style={styles.loginContainer}>
+                        <StatusBar style="auto" />
+                        <View style={styles.inputView}>
+                        <TextInput
+                           style={styles.TextInput}
+                           placeholder="Email."
+                           placeholderTextColor="#003f5c"
+                           onChangeText={(email) => setEmail(email)}
+                        /> 
+                        </View> 
+                        <View style={styles.inputView}>
+                        <TextInput
+                           style={styles.TextInput}
+                           placeholder="Password."
+                           placeholderTextColor="#003f5c"
+                           secureTextEntry={true}
+                           onChangeText={(password) => setPassword(password)}
+                        /> 
+                        </View> 
+                        <TouchableOpacity>
+                        <Text style={styles.forgot_button}>Forgot Password?</Text> 
+                        </TouchableOpacity> 
+                        <TouchableOpacity style={styles.loginBtn}>
+                        <Text 
+                           style={styles.loginText} 
+                          >LOGIN</Text> 
+                          
+                        </TouchableOpacity> 
+                        <Text style ={{
+                           marginTop: 10,
 
+                        }}>No account yet? 
+                           <Text >Register</Text>
+                          </Text>
+                     </View>
+            </View>
+
+            <View style ={{
+               width: '100%',
+               height: '8%',
+               backgroundColor: '#006A4E',
+               marginTop: '31%',
+               borderWidth: 1,
+               borderColor: "thistle",
+               borderRadius: 2,
+               flexDirection: 'row',
+               alignContent: 'space-between',
+               
+            }} >
+               <Text style ={{
+                  padding:10,
+                  paddingLeft:40,
+                  paddingRight:40,
+                  color: '#fff',
+                  fontSize: 20,
+               }}>Home</Text>
+               <Text style ={{
+                  padding:10,
+                  paddingLeft:40,
+                  paddingRight:40,
+                  color: '#fff',
+                  fontSize: 20,
+               }}>Search</Text>
+               <Text style ={{
+                  padding:10,
+                  paddingLeft:20,
+                  paddingRight:20,
+                  color: '#fff',
+                  fontSize: 20,
+               }}>Post</Text>
+
+            </View>
 
             
-            {/* <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-            <View style={{flex: 3, backgroundColor: 'steelblue'}} />
-            <View style={{flex: 4, backgroundColor: 'skyblue'}} />
-            <View style={{flex: 5, backgroundColor: 'steelblue'}} /> */}
           </View>
     
    )
@@ -67,7 +150,43 @@ const styles = StyleSheet.create ({
      borderWidth: 1,
      padding: 25,
      borderColor: 'black',
-     backgroundColor: 'yellow',
+     backgroundColor: 'red',
     
-  }
+  },
+  loginContainer: {
+   flex: 1,
+   backgroundColor: "#fff",
+   alignItems: "center",
+   justifyContent: "center",
+ },
+ inputView: {
+   backgroundColor: "#fff",
+   borderBottomColor: 'gray',
+   borderBottomWidth: 2,
+   marginBottom: 30,
+   width: "70%",
+   height: 45,
+   marginBottom: 20,
+   
+ },
+ TextInput: {
+   height: 50,
+   flex: 1,
+   padding: 10,
+   marginLeft: 20,
+ },
+ forgot_button: {
+   height: 30,
+   marginBottom: 10,
+ },
+ loginBtn: {
+   width: "60%",
+   borderRadius: 25,
+   height: 50,
+   alignItems: "center",
+   justifyContent: "center",
+   marginTop: 10,
+   backgroundColor: "#4287f5",
+ },
+ 
 })
